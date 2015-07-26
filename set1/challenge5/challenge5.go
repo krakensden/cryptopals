e7b43bc0c5f38e7d18bb664a76644a27710aac09
kd <krakensden@krakensden.com>
2015-07-26 13:19:22 -0700

    I was confused by the challenge description
    Linebreaks are real in source text, fake in correct output

--- set1/challenge5/challenge5.go
+++ set1/challenge5/challenge5.go
@@ -6,11 +6,9 @@ import (
 )
 
 func main() {
-	demo0 := "Burning 'em, if you ain't quick and nimble"
-	demo1 := "I go crazy when I hear a cymbal"
+	demo0 := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 
-	success0 := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
-	success1 := "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+	success0 := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
 	output0, err := stringxor.StringXor(demo0, "ICE")
 	if err != nil {
@@ -20,12 +18,5 @@ func main() {
 
 	fmt.Println("First line succeeded:", output0 == success0)
 	fmt.Println("Output0: ", output0)
-
-	output1, err := stringxor.StringXor(demo1, "ICE")
-	if err != nil {
-		fmt.Println("Error in XOR process ", err)
-		return
-	}
-	fmt.Println("First line succeeded:", output1 == success1)
-	fmt.Println("Output1: ", output1)
+	fmt.Println("len(Output0): ", len(output0), " len(dem0): ", len(demo0), " len(success0): ", len(success0))
 }
--- set1/challenge5/stringxor/stringxor.go
+++ set1/challenge5/stringxor/stringxor.go
@@ -3,13 +3,11 @@ package stringxor
 import (
 	"../../challenge1/hex2b64"
 	"../../challenge2/bytexor"
-	"fmt"
 )
 
 func SliceXor(text, key []byte) ([]byte, error) {
 	full_key := make([]byte, len(text), len(text))
 	for index, _ := range text {
-		fmt.Println("index", index)
 		full_key[index] = key[index%len(key)]
 	}
 	return bytexor.SliceXor(text, full_key)
