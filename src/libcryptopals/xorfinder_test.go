7c3f3ce0818271772701b8366052186f539e7b04
kd <krakensden@krakensden.com>
2015-10-12 15:13:41 -0700

    Fix missing input from poor refactor

--- src/libcryptopals/xorfinder_test.go
+++ src/libcryptopals/xorfinder_test.go
@@ -3,7 +3,7 @@ package libcryptopals
 import "testing"
 
 func TestXorFinder(t *testing.T) {
-	best_result := XorFinderDecodeFile("../challenge4/input.txt")
+	best_result := XorFinderDecodeFile("../set1/challenge4/input.txt")
 	if best_result == nil {
 		t.Errorf("Failed to parse the input")
 		return
