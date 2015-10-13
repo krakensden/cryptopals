18c014262075efcc009568c2a886c5318a16973a
kd <krakensden@krakensden.com>
2015-10-12 18:32:03 -0700

    Verify my xor impl is actually reversible

--- src/set1/challenge5/challenge5.go
+++ src/set1/challenge5/challenge5.go
@@ -15,8 +15,20 @@ func main() {
 		fmt.Println("Error in XOR process ", err)
 		return
 	}
+	successBytes, err := libcryptopals.Hex2Byte(success0)
+	if err != nil {
+		fmt.Println("Couldn't decode success0")
+		return
+	}
+	output1, err := libcryptopals.SliceRepeatingXor(successBytes, []byte("ICE"))
+	if err != nil {
+		fmt.Println("Error in XOR process ", err)
+		return
+	}
 
 	fmt.Println("First line succeeded:", output0 == success0)
 	fmt.Println("Output0: ", output0)
 	fmt.Println("len(Output0): ", len(output0), " len(dem0): ", len(demo0), " len(success0): ", len(success0))
+	fmt.Println("& reverse: ", string(output1))
+	fmt.Println("& reverse == success0 ", demo0 == string(output1))
 }
